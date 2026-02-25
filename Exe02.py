1290b970174d5d1388138d38be57a69346014c5c
pedro_sergio <franca.pedro@ort.org.br>
2026-02-25 15:30:46 -0300

    Exercício2

---
+++ Exe02.py
@@ -0,0 +1,21 @@
+'''
+2. Crie um programa que leia as quatro notas de um aluno e mostra na tela a sua média final.
+Exemplo: 
+N1 = 6,0
+N2 = 7,5
+N3 = 5,5
+N4 = 10
+Média = 7,25
+'''
+
+#ENTRADA
+n1 = float(input('Informe a nota 01:'))
+n2 = float(input('Informe a nota 02:'))
+n3 = float(input('Informe a nota 03:'))
+n4 = float(input('Informe a nota 04:'))
+
+#PROCESSAMENTO
+media = (ni + n2 + n3 + n4) / 4
+
+#SAÍDA
+print('Media final:', media) 
