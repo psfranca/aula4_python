532001fb0f1b0c3052c31a6ffec76a19747eb57a
pedro_sergio <franca.pedro@ort.org.br>
2026-02-25 15:03:43 -0300

    Finalizando o exercício 1

---
+++ Exe01.py
@@ -0,0 +1,13 @@
+'''
+1. Crie um programa que leia o salário de um funcionário e mostra na tela o seu salário
+anual.
+'''
+
+
+
+
+#ENTRADA
+#CAPTURANDO O SALÁRIO E CONVERTENDO PARA FLOAT
+salário_mensal = float(input('informe o seu salário mensal:'))
+
+#PROCESSAMENTO
